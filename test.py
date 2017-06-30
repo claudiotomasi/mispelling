9bf277c9fdb2bc8221ae3739b913737d60a5c348
cla <cla@debian>
2017-06-30 16:33:09 +0200

    37mila nuovi tweet

--- test.py
+++ test.py
@@ -4,46 +4,43 @@ import hidden_markov as hmlib
 import codecs, pickle, argparse, glob
 from sklearn import metrics
 import numpy as np
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--train', help='Training or not', action='store_true')
-args = parser.parse_args()
-if args.train:
-    print "With Training"
-    prior = hmm_init.prior_probability()
-    transitions = hmm_init.transition_model()
-    states=hmm_init.states()
-    possible_obs = hmm_init.observation()
-    emissions = hmm_init.emission_probability(utility.adjacents())
-    model = hmlib.hmm(states, possible_obs, prior, transitions, emissions)
-    afile = open('training/model', 'wb')
-    pickle.dump(model, afile)
-    afile.close()
-    print "End of training\n"
-
-else:
-    print "Without Training\n"
-    afile = open('training/model', 'rb')
-    model = pickle.load(afile)
-    afile.close()
-#print len(states),len(emissions)
+from flask import Flask, request, jsonify, render_template
 
 
-states = model.states
-####
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--train', help='Training or not', action='store_true')
+    args = parser.parse_args()
+    if args.train:
+        print "With Training"
+        prior = hmm_init.prior_probability()
+        transitions = hmm_init.transition_model()
+        states=hmm_init.states()
+        possible_obs = hmm_init.observation()
+        emissions = hmm_init.emission_probability(utility.adjacents())
+        model = hmlib.hmm(states, possible_obs, prior, transitions, emissions)
+        afile = open('training/model', 'wb')
+        pickle.dump(model, afile)
+        afile.close()
+        print "End of training\n"
 
-for filename in glob.glob('remain/Pontifex_test_of_remains.txt'):
-    tweet = ""
-    with codecs.open(filename , "r", 'utf-8-sig') as f:
-        print "Start prediction of "+filename+"..."
-        for tweets in f:
-            tweet += hmm_init.convert(tweets)
-    #elimino newline
-    tweet = tweet.replace('\n','')
-    #separo per frasi
+    else:
+        print "Without Training\n"
+        afile = open('training/model', 'rb')
+        model = pickle.load(afile)
+        afile.close()
+    for filename in glob.glob('remain/Pontifex_test_of_remains.txt'):
+        tweet = ""
+        with codecs.open(filename , "r", 'utf-8-sig') as f:
+            print "Start prediction of "+filename+"..."
+            for tweets in f:
+                tweet += hmm_init.convert(tweets)
+        #elimino newline
+        tweet = tweet.replace('\n','')
+        #separo per frasi
 
 
-    # list_of_words = tweet.split('.')
+        # list_of_words = tweet.split('.')
     list_of_words = tweet.split()
 
     correct = ""
@@ -64,27 +61,7 @@ for filename in glob.glob('remain/Pontifex_test_of_remains.txt'):
     #trasformo lista predizioni in stringa
 
         correct += ''.join(pred)
-
     with codecs.open(filename+'_correct', 'w', 'utf-8-sig') as f:
         correct = unicode(correct)
         f.write(correct)
     print "End prediction of "+filename+"\n"
-
-# real=[]
-# tweet=''
-# with codecs.open('remain/Pontifex_test_of_remains.txt', 'r', 'utf-8-sig') as f:
-#     for tweets in f:
-#         tweet += hmm_init.convert(tweets)
-#     tweet = tweet.replace('\n','')
-# real = list(tweet)
-#
-#
-# tweet=''
-# pred = []
-# with codecs.open('test/Pontifex_test_of_remains.txt_correct', 'r', 'utf-8-sig') as f:
-#     for tweets in f:
-#         tweet += hmm_init.convert(tweets)
-#
-# pred = list(tweet)
-# print metrics.confusion_matrix(real, pred, states, sample_weight=None)
-#Si potrebbe calcolare viterbi sulle parole e non sulle frasi
